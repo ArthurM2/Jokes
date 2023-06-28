d9ea01712ce14d17ed52fb2b0463490e5c436957
ArthurM2 <38484496+ArthurM2@users.noreply.github.com>
2023-06-28 14:17:04 +0500

    - Разделил все на папки для более удобной навигации по проекту - Добавил цвета - Добавил лончскрин

--- Joke-Generator/Helpers/UIColor+Extensions.swift
+++ Joke-Generator/Helpers/UIColor+Extensions.swift
@@ -8,5 +8,9 @@
 import UIKit
 
 extension UIColor {
-    
+    static var ypBlack: UIColor { UIColor(named: "YP Black") ?? UIColor.black }
+    static var ypWhite: UIColor { UIColor(named: "YP White") ?? UIColor.white }
+    static var ypGreen: UIColor { UIColor(named: "YP Green") ?? UIColor.green }
+    static var ypGray: UIColor { UIColor(named: "YP Gray") ?? UIColor.gray }
+    static var ypPink: UIColor { UIColor(named: "YP Pink") ?? UIColor.purple }
 }
